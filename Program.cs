bff162444299b0f773e99790f8986f13b3df473e
Faizan <faizanali2860@gmail.com>
2025-09-15 17:41:12 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("How many motors are carrying the packages?");
+var motors =     int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("How many kg of packages do we expect?");
+var weight = double.Parse(Console.ReadLine()!);
+
+var quotient = weight / motors;
+if (quotient <= 5.6)
+{
+    Console.WriteLine("Yes! The conveyor belt can carry the packages.");
+}
+else
+{
+    Console.WriteLine("No. The conveyor belt cannot carry the packages.");
+    
+    
+}
